a171d1837f25e7f90eae1583ea039e17034200b7
Maxime Crampon <maxime.crampon@gmail.com>
2019-07-04 09:42:17 +0200

    add URL to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(name="sike_strat_draw",
         version="0.1",
-        description="Tool to draw SIKE or other isogeny-based crypotgraphic protocols strategies",  
+        description="Tool to draw SIKE or other isogeny-based crypotgraphic protocols strategies", 
+        url="https://github.com/MaximeCrp/sike_strat_draw",
         author="Maxime Crampon",
         author_email="maxime.crampon@protonmail.com",
         packages=["sike_strat_draw"])
