7a7072ec95dc621eb99fbf8fcf5a77e0a35b3dd4
MaximeCrp <maxime.crampon@gmail.com>
2019-07-09 10:54:52 +0200

    add docstring and _private methods indication

--- sike_strat_draw/drawer.py
+++ sike_strat_draw/drawer.py
@@ -2,8 +2,114 @@ import turtle
 import canvasvg
 import datetime
 
+"""
+sike_strat_drawer module
+========================
+
+This module is meant to be a tool to allow easier representation of isogenies computations in the form of trees.
+It can be used in the SIKE protocol context to represent different computation strategies.
+
+Class
+-----
+Drawer : 
+    the very class used to handle the screen and graph. It uses the turtle module implemented thanks to tkinter canvas
+
+
+Dependencies
+------------
+turtle :
+    module used to actually draw the graph
+canvasvg :
+    community tool to save tkinter canva into a svg file
+datetime :
+    for default file name saving with current date and time
+    
+:Example:
+
+>> from sike_strat_draw import drawer
+>> import random
+>> dw = drawer.Drawer()
+>> dw.init_drawing()
+>> dw.save_point(0, 0)
+>> dw.draw_isogeny()
+>> dw.draw_final_dot()
+>> dw.restore_point(0, 0)
+>> dw.draw_doubling()
+>> dw.draw_final_dot()
+>> dw.end_tree()
+>> dw.save_drawing()
+>> dw.end_drawing()
+
+"""
+
 
 class Drawer:
+    """
+        A class used to draw isogenic strategy trees. Refer to SIKE protocol documentation
+
+        ...
+
+        Attributes
+        ----------
+        :param tree_angle: default 30, in degrees, (half) the angle between each branch split
+        :param arrow_length: default 50, the length of each branch. Needs to be small for large trees
+        :param arrow_head_length: default 5, the length of the head of each branch as an arrow
+        :param arrow_head_angle: default 135, in degrees, the angle for arrow head representation
+        :param dot_radius: default 4, radius of dots drawn to represent elliptic curve points
+        :param isogeny_color: default "red", color of branches representing isogeny computations
+        :param doubling_color: default "blue", color of branches representing doubling computations
+        :param dot_color: default "black", color of standard dots representing standard points
+        :param final_dot_color: default "green", color of dots representing destination points
+        :param screen_width_ratio: default 1.0, floating number for screen width ratio
+        :param screen_height_ratio: default 1.0, floating number for screen height ratio
+        :param drawing_speed: default 20, integer driving the pace of drawing
+        :param save_file_name: default None, custom file name for canvas saving, defaults to current date/time
+
+        Methods
+        -------
+        save_point(row, index)
+            Saves graph coordinates for (row, index) elliptic curve point
+
+        restore_point(row, index)
+            Moves drawing coordinates to previously saved (row, index) curve point
+
+        init_drawing()
+            Initiates screen, turtle and first point
+
+        _init_isogeny()
+            Initiates color and branch splitting for isogeny
+
+        _init_doubling()
+            Initiates color and branch splitting for isogeny
+
+        _init_arrow()
+            Moves outside previous dot before drawing arrow
+
+        _draw_arrow()
+            Draws arrow in previously set position, angle and color
+
+        _draw_dot()
+            Draws standard dot
+
+        draw_isogeny()
+            Sets correct angle and color then draws isogeny arrow
+
+        draw_doubling()
+            Sets correct angle and color then draws doubling arrow
+
+        draw_final_dot()
+            Draws dot corresponding to destination curve point
+
+        end_tree()
+            Hides turtle drawer at the end
+
+        save_drawing()
+            Saves whole canvas to a file with name set to save_file_name
+
+        end_drawing()
+            Static method. Ends drawing session
+        """
+
     def __init__(
         self,
         tree_angle=30,
@@ -47,17 +153,31 @@ class Drawer:
         self.point_dict = {}
 
     def save_point(self, row, index):
+        """
+        Saves graph coordinates for (row, index) elliptic curve point.
+        A point which has been saved one time can't have it's graph coordinates changed afterwards.
+
+        :param row: row of curve point in drawn strategy
+        :param index: index of curve point in drawn strategy
+        """
         if (row, index) in self.point_dict:
             print("Already in dict!")
         else:
             self.point_dict.update({(row, index): self.painter.pos()})
 
     def restore_point(self, row, index):
+        """
+        Move drawing coordinates to previously saved (row, index) curve point
+
+        :param row: row of curve point in drawn strategy
+        :param index: row of curve point in drawn strategy
+        """
         self.painter.penup()
         self.painter.setposition(self.point_dict.get((row, index)))
         self.painter.pendown()
 
     def init_drawing(self):
+        """Initiates screen, turtle and first point"""
         self.screen = turtle.Screen()
         self.screen.screensize()
         self.screen.setup(width=self.screen_width_ratio, height=self.screen_height_ratio)
@@ -74,22 +194,26 @@ class Drawer:
 
         self.painter.dot(2 * self.dot_radius, self.dot_color)
 
-    def init_isogeny(self):
+    def _init_isogeny(self):
+        """Initiates color and branch splitting for isogeny"""
         self.painter.pencolor(self.isogeny_color)
         self.painter.left(self.tree_angle)
         self.number_isogenies = self.number_isogenies + 1
 
-    def init_doubling(self):
+    def _init_doubling(self):
+        """Initiates color and branch splitting for isogeny"""
         self.painter.pencolor(self.doubling_color)
         self.painter.right(self.tree_angle)
         self.number_doubling = self.number_doubling + 1
 
-    def init_arrow(self):
+    def _init_arrow(self):
+        """Moves outside previous dot before drawing arrow"""
         self.painter.penup()
         self.painter.forward(self.dot_radius)
         self.painter.pendown()
 
-    def draw_arrow(self):
+    def _draw_arrow(self):
+        """Draws arrow in previously set position, angle and color"""
         self.painter.forward(self.arrow_length)
         arrow_head_origin = self.painter.pos()
         self.painter.left(self.arrow_head_angle)
@@ -100,31 +224,37 @@ class Drawer:
         self.painter.setposition(arrow_head_origin)
         self.painter.left(self.arrow_head_angle)
 
-    def draw_dot(self):
+    def _draw_dot(self):
+        """Draws standard dot"""
         self.painter.forward(self.dot_radius)
         self.painter.dot(2 * self.dot_radius, self.dot_color)
 
     def draw_isogeny(self):
-        self.init_isogeny()
-        self.init_arrow()
-        self.draw_arrow()
-        self.draw_dot()
+        """Sets correct angle and color then draws isogeny arrow"""
+        self._init_isogeny()
+        self._init_arrow()
+        self._draw_arrow()
+        self._draw_dot()
         self.painter.right(self.tree_angle)
 
     def draw_doubling(self):
-        self.init_doubling()
-        self.init_arrow()
-        self.draw_arrow()
-        self.draw_dot()
+        """Sets correct angle and color then draws doubling arrow"""
+        self._init_doubling()
+        self._init_arrow()
+        self._draw_arrow()
+        self._draw_dot()
         self.painter.left(self.tree_angle)
 
     def draw_final_dot(self):
+        """Draws dot corresponding to destination curve point"""
         self.painter.dot(2 * self.dot_radius, self.final_dot_color)
 
     def end_tree(self):
+        """Hides turtle drawer at the end"""
         self.painter.hideturtle()
 
     def save_drawing(self):
+        """Saves whole canvas to a file with name set to save_file_name"""
         if self.save_file_name is None:
             now = datetime.datetime.now()
             self.save_file_name = now.strftime("strat_%Y%m%d-%H%M%S.svg")
@@ -132,5 +262,6 @@ class Drawer:
 
     @staticmethod
     def end_drawing():
+        """Static method. Ends drawing session"""
         turtle.done()
 
